cb28683e49020aaf7d4a103c979464e2501fdb5b
karthik <karthik18tech@gmail.com>
2026-04-08 22:43:37 +0530

    first commit

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.API_URI;
+const VITE_API_URI = import.meta.env.VITE_API_URI;
 
 // get products
 export const getAllProducts = async (page, limit, category) => {
   try {
-    const response = await axios.get(`${API_URL}`, {
+    const response = await axios.get(`${VITE_API_URI}`, {
       params: { page, limit, category },
     });
     return response.data;
@@ -21,7 +21,7 @@ export const getAllProducts = async (page, limit, category) => {
 // create product
 export const createProduct = async (formData) => {
   try {
-    const response = await axios.post(API_URL, formData);
+    const response = await axios.post(VITE_API_URI, formData);
     return response.data;
   } catch (error) {
     console.error(
@@ -35,7 +35,7 @@ export const createProduct = async (formData) => {
 // update product
 export const updateProduct = async (id, data) => {
   try {
-    const response = await axios.put(`${API_URL}${id}`, data);
+    const response = await axios.put(`${VITE_API_URI}${id}`, data);
     return response.data;
   } catch (error) {
     console.error(
@@ -49,7 +49,7 @@ export const updateProduct = async (id, data) => {
 // delete product
 export const deleteProduct = async (id) => {
   try {
-    const response = await axios.delete(`${API_URL}${id}`);
+    const response = await axios.delete(`${VITE_API_URI}${id}`);
     return response.data;
   } catch (error) {
     console.error(
